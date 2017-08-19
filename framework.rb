3e63d0e7c58dcaf57e68515b09619c460fb24a73
WilfredTA <rockinghamtannr@gmail.com>
2017-08-19 16:47:26 -0400

    Adding comments to explain methods

--- framework.rb
+++ framework.rb
@@ -3,11 +3,13 @@ class CustomFrame
     @routes={}
     @error_response = []
   end
+  
   def response(status, headers, body='')
     body =  yield if block_given?
     [status, headers,[body]]
   end
-
+  
+  # Stores the return value of loading an erb template in a local variable use_template and then loads use_template  within a second layout template
   def erb(filename, locals={}, layout=nil)
     b = binding
     use_template =  template(filename, locals)
@@ -21,7 +23,7 @@ class CustomFrame
   end
 
 
-  
+  # Stores variables to display in a local variae to_display and loads them within an erb template
   def template(filename, locals={})
     b = binding
     to_display = locals[:display]
@@ -30,6 +32,7 @@ class CustomFrame
     ERB.new(content).result(b)
   end
 
+  # Checks if Http request matches a route in the @routes array and returns a reaponse with the body specified in the matched route. Returns  an error message ir no routes in @routes matches request.
   def route(route_info, status='200', headers={}\
 )
     method = route_info[0]
@@ -44,14 +47,17 @@ class CustomFrame
     [status, headers,[body]]
   end
 
+  # Add a route to the @routes array
   def add_route(method, path, &block)
     @routes[[method, path]] = block
   end
 
+  # Returns the error response
   def error
      @error_response
   end
 
+  # Stores the return value of the passed in block as the body of the error response. Returns an array containing the status, headers, and the return value of the block.
   def set_error(status, headers={})
     error_response =  yield if block_given?
     @error_response = [status, headers,[error_response]]
